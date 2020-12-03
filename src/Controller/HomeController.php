3212780788f765d6738eff681abded029ca761e8
Xavier Laviron <xavier@norival.dev>
2020-12-03 22:36:16 +0100

    style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends AbstractController
     public function homePage(): Response
     {
         return $this->render('home/index.html.twig', [
+            'title' => 'Camille Puverel',
         ]);
     }
 }
