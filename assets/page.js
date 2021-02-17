93ab143303d0e1aaac5febfcf8c3ed305c9fe061
Xavier Laviron <xavier@norival.dev>
2021-02-17 11:52:53 +0100

    responsive menu style and behaviour

--- assets/page.js
+++ assets/page.js
@@ -1,5 +1,15 @@
 import './styles/page.scss';
 
+/**
+ * @param {Event} event
+ * @returns {undefined}
+ */
+const onClickMenuToggle = (event) => {
+    event.preventDefault();
+    document.getElementById('responsive-menu').classList.toggle('active');
+    document.getElementById('menu-toggle').classList.toggle('active');
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     let lastScrollPosition = 0;
     let ticking = false;
@@ -8,13 +18,13 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', () => {
         if (!ticking) {
             window.requestAnimationFrame(() => {
-                if (lastScrollPosition > window.scrollY) {
-                    // going up
-                    header.style.transform = 'translateY(0)';
-                } else {
-                    // going down
-                    header.style.transform = 'translateY(-100%)';
-                }
+                // if (lastScrollPosition > window.scrollY) {
+                //     // going up
+                //     header.style.transform = 'translateY(0)';
+                // } else {
+                //     // going down
+                //     header.style.transform = 'translateY(-100%)';
+                // }
                 lastScrollPosition = window.scrollY;
 
                 if (lastScrollPosition > 0) {
@@ -29,4 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
             ticking = true;
         }
     });
+
+    // Burger menu
+    document.getElementById('menu-toggle').addEventListener('click', onClickMenuToggle);
 });
